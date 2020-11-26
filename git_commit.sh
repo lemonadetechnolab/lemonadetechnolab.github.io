c8db2871b888c26f9686d0d7852a7a58f771068c
lemonadetechnolab <uni.maxim.velli@gmail.com>
2020-11-26 10:28:37 +0100

    Thu Nov 26 10:28:37 CET 2020

--- git_commit.sh
+++ git_commit.sh
@@ -5,11 +5,7 @@ git config --global user.email "uni.maxim.velli@gmail.com"
 git config --global github.user "lemonadetechnolab"
 git config --global github.token "a20a6c2a248bbf1b3449f04df53030ee8440eec7"
 
-while True
-do
-	desc=`date`
-	git add .  
-	git commit -m "$desc"
-	git push origin main
-	sleep 10
-done
+desc=`date`
+git add .  
+git commit -m "$desc"
+git push origin main
