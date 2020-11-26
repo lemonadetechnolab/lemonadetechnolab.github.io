d2f50fbccd02dcc43f56de9c564af5f2f91aeb19
lemonadetechnolab <uni.maxim.velli@gmail.com>
2020-11-26 11:05:21 +0100

    Thu Nov 26 11:05:21 CET 2020

--- git_commit.sh
+++ git_commit.sh
@@ -8,4 +8,4 @@ git config --global github.token "a20a6c2a248bbf1b3449f04df53030ee8440eec7"
 desc=`date`
 git add .  
 git commit -m "$desc"
-git push
+git push git@github.com:lemonadetechnolab/lemonadetechnolab.github.io.git main
